4fb5af4ee874fea4169953969ef25d3bd6601711
MOBIUS <yuchenliu0710@outlook.com>
2026-01-18 01:01:12 -0800

    implement MazeRoom

--- Assets/Scripts/Maze/MazeRoom.cs
+++ Assets/Scripts/Maze/MazeRoom.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class MazeRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Member variables
+    // Walls with order of North, East, South, West
+    [SerializeField] private GameObject[] MidWalls;
+    [SerializeField] private GameObject[] ArcWalls;
+    
+    // Change MidWall to ArcWall
+    // direction must be one of "North", "East", "South", "West"
+    public void ChangeWall(string direction)
     {
+        // Determine index
+        int index;
+        if (direction == "North")
+        {
+            index = 0;
+        }
+        else if (direction == "East")
+        {
+            index = 1;
+        }
+        else if (direction == "South")
+        {
+            index = 2;
+        }
+        else if (direction == "West")
+        {
+            index = 3;
+        }
+        else
+        {
+            index = -1;
+            Debug.LogError("Input direction invalid");
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Change wall
+        MidWalls[index].SetActive(false);
+        ArcWalls[index].SetActive(true);
     }
 }
